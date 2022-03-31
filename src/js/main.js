bcd4ca6ebec37332be0833ac0e6e7f95caa92c7e
WagnerMor <wagnermor@outlook.com>
2022-03-30 23:37:11 -0300

    add: src directory and new repairs

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+let inputEmailReq = document.querySelector('#inputEmail')
+let inputPasswordReq = document.querySelector('#inputPassword')
+let botaoSubmitRef = document.querySelector('button')
+
+
+botaoSubmitRef.addEventListener('click', event => {
+
+    event.preventDefault()
+    
+
+
+})
---
+++ src/js/signup.js
@@ -0,0 +1,13 @@
+let inputNomeRef = document.querySelector('#inputNome')
+let inputApelidoRef = document.querySelector('#inputApelido')
+let inputEmailRef = document.querySelector('#inputEmail')
+let inputSenhaRef = document.querySelector('#inputSenha')
+let botaoCriarRef = document.querySelector('criar')
+
+botaoSubmitRef.addEventListener('click', event => {
+
+    event.preventDefault()
+
+    console.log(inputCepRef.value)
+
+})
