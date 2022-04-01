40c1dc09456dd13ec7b53b7b848339560dbdb3e7
WagnerMor <wagnermor@outlook.com>
2022-03-31 22:50:55 -0300

    fix: validation index.html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
 let inputEmailReq = document.querySelector('#inputEmail')
 let inputPasswordReq = document.querySelector('#inputPassword')
 let botaoSubmitRef = document.querySelector('button')
+const teste  = document.querySelectorAll('[required]');
+
+console.log(teste);
 
 
 botaoSubmitRef.addEventListener('click', event => {
 
     event.preventDefault()
-    
+
+    for(let input of teste) {
+        if(input.value === "") {
+            input.style.border = "1px solid red"
+            input.classList.toggle("span-error")
+        }
+    }
 
 
 })
