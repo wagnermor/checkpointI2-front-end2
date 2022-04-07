5de5cff488497b2e0cf923de817a418e4d479c07
Elivelton A Viveiro <82124588+Loretto-dev@users.noreply.github.com>
2022-04-06 22:58:11 -0300

    Update: add tasks on tarefas.js

--- js/tarefas.js
+++ js/tarefas.js
@@ -1,4 +1,16 @@
 let botaocloseAppRef = document.querySelector('#closeApp')
+let addTaskRef = document.querySelector('#addTask')
+let not-doneRef = document.querySelector('.not-done')
+let addTaskRef = document.querySelector('.descricao')
+let addTaskRef = document.querySelector('.nome')
+let addTaskRef = document.querySelector('.timestampk')
+
+
+<div class="not-done"></div>
+<div class="descricao">
+  <p class="nome">Nova tarefa</p>
+  <p class="timestamp">Criada em: 15/07/21</p>
+</div>
 
 let resquestConfigurationReq = {
 
@@ -9,4 +21,60 @@ let resquestConfigurationReq = {
     }
 }
 
-fetch('https://ctd-todo-api.herokuapp.com/v1/users/getMe',resquestConfigurationReq)
+addTaskRef.addEventListener('click',  event => {
+
+    event.preventDefault()
+
+    let users = {
+
+            id: 1,
+            description: ,
+            completed: false,
+            userId: 1,
+            createdAt: "2021-06-30T22:53:09.549Z"
+        
+    }
+
+    fetch('https://ctd-todo-api.herokuapp.com/v1/users/tasks', resquestConfigurationReq).then(
+    
+    response => {
+        console.log(response)
+
+        if(response.ok) {
+
+            let skeletonRef = document.querySelector('#skeleton')
+
+            skeletonRef.style.display = 'none'
+
+            response.json().then(
+                
+                tasks => {
+                    `<li class="tarefa">
+                        <div class="not-done"></div>
+                        <div class="descricao">
+                            <p class="nome">Nova tarefa</p>
+                            <p class="timestamp">Criada em: 15/07/21</p>
+                        </div>
+                    </li>`
+                }
+            )
+
+        } else {
+            if(response.status !== 401) {
+
+                logOutUser()
+
+            }
+        }
+    })
+
+
+})
+
+
+
+
+
+
+
+
