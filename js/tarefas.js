0a3a5fd6fb2c4bf6d1b0aa93bd01ac365bc3da7a
Elivelton A Viveiro <82124588+Loretto-dev@users.noreply.github.com>
2022-04-15 23:35:07 -0300

    Feat: changes functions creatTasks, getTask and include functions updateTask and deleteTask

--- js/tarefas.js
+++ js/tarefas.js
@@ -3,8 +3,11 @@ let userImageRef = document.querySelector('#userImage')
 let closeAppRef = document.querySelector('#closeApp')
 let novaTarefaRef = document.querySelector('#novaTarefa')
 let addTaskRef = document.querySelector('#addTask')
-let taskList = []
-let tasksCompletedList = []
+let listUnfinishedTasksRef = document.querySelector('.tarefas-pendentes')
+let listFinishedTasksRef = document.querySelector('.tarefas-terminadas')
+// let removeSkeletonRef = document.querySelector('#skeleton')
+
+
 
 //Não volta o login e apaga os itens
 if(localStorage.getItem('token') === null){
@@ -12,16 +15,20 @@ if(localStorage.getItem('token') === null){
 }
 console.log(localStorage.getItem('token'))
 
+
+
 //requisitar configuração do token
-let resquestConfigurationReq = {
+let requestHeaders = {
     headers: {
         'Content-Type': 'application/json',
-        Authorization: localStorage.getItem('token')
+        'Authorization': localStorage.getItem('token')
     }
 }
 
+
+
 //Mostrar nome do Usuario
-fetch('https://ctd-todo-api.herokuapp.com/v1/users/getMe', resquestConfigurationReq).then(
+fetch('https://ctd-todo-api.herokuapp.com/v1/users/getMe', requestHeaders).then(
     response => response.json().then(
         data => {
             nome = data.firstName
@@ -31,109 +38,178 @@ fetch('https://ctd-todo-api.herokuapp.com/v1/users/getMe', resquestConfiguration
     )
 )
 
+
+
 //Limpar pagina tarefas
 closeAppRef.addEventListener('click',  event => {
     localStorage.clear()
     window.location.href = './index.html'
 })
 
+
+
+
+//Função de cricação de Tarefas
+function creatTask(){   
+    let newTask = {
+        description: novaTarefaRef.value,
+        completed: false
+    }
+    let requestHeaders = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('token')
+        }
+    }
+    let requestConfigurationPost = {
+        method: "POST",
+        body: JSON.stringify(newTask),
+        headers: requestHeaders
+    }
+    fetch("https://ctd-todo-api.herokuapp.com/v1/tasks", requestConfigurationPost).then(response => {
+        response.json()
+    })
+
+
+
+}
+
 //Botao adicionar tarefas
 addTaskRef.addEventListener('click',  event => {
 
     event.preventDefault()
 
-    //Função de cricação de Tarefas
-    function creatTask(){   
-        let newTask = {
-            description: novaTarefaRef,
-            completed: false
+    creatTask();
+
+
+})
+
+
+//Lista de tarefas
+function getTasks() {
+    let requestHeaders = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('token')
         }
-        let requestConfigurationPost = {
-            method: "POST",
-            body: JSON.stringify(newTask),
-            headers: requestHeaders
-        }
-        fetch("https://ctd-todo-api.herokuapp.com/v1/tasks", requestConfigurationPost).then(response => {
-            response.json()
-        })
     }
+    let task = {
+        id: 1,
+        description: novaTarefaRef,
+        completed: false,
+        userId: 1,
+        createdAt: "2021-06-30T22:53:09.549Z"
+    }
+    fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', requestHeaders).then(
+        response => {
+            if(response.ok) {
 
-    /********************************************************** */
+                response.json().then(
 
-    //Lista de tarefas
-    function getTasks() {
-
-        let task = {
-            id: 1,
-            description: false,
-            completed: false,
-            userId: 1,
-            createdAt: "2021-06-30T22:53:09.549Z"
-        }
-        let requestHeaders = {
-            'Content-Type': 'application/json'
-        }
-        let requestGetAuthorizateConfiguration = {
-            method: 'POST',
-            body: JSON.stringify(task),
-            headers: requestHeaders
-        }
-        fetch('https://ctd-todo-api.herokuapp.com/v1/tasks', requestGetAuthorizateConfiguration).then(
-            response => {
-                if(response.ok) {
-                    response.json().then(
-                        tasks => {
-                            renderTasks(tasks)
-
+                    tasks => {
+                        console.log(tasks.length)
+                        if(tasks.length >= 1){
+                            //removeSkeletonRef.remove('skeleton');
+                            listFinishedTasksRef.innerHTML = ''
                             listUnfinishedTasksRef.innerHTML = ''
+                        } 
 
-                            for(let task of tasks) {
 
+
+
+                        for(let currentTask of tasks) {
+
+                            const dataFormatada = new Date(task.createdAt).toLocaleDateString(
+                                'pt-BR',
+                                {
+                                    day: '2-digit',
+                                    month: '2-digit',
+                                    year: 'numeric'
+                                }
+                            )
+                            
+                            if(currentTask.completed === false){
                                 listUnfinishedTasksRef.innerHTML += `
                                     <li class="tarefa">
-                                        <div class="not-done" onclick="updateTask(${task.id})"></div>
+                                        <div class="not-done" onclick="updateTask(${currentTask.id})"></div>
                                         <div class="descricao">
-                                        <p class="nome">${task.description}</p>
-                                        <p class="timestamp">Criada em:${task.createdAt}</p>
+                                            <p class="nome">${currentTask.description}</p>
+                                            <p class="timestamp">Criada em:${dataFormatada}</p>
+                                        </div>
+                                    </li>`
+
+                            }else{
+                                    listFinishedTasksRef.innerHTML += `
+                                    <li class="tarefa">
+                                        <div class="not-done" onclick="updateTask(${currentTask.id})"></div>
+                                        <div class="descricao">
+                                            <p class="nome">${currentTask.description}</p>
+                                            <p class="timestamp">Criada em:${dataFormatada}</p>
                                         </div>
                                     </li>`
                             }
                         }
-                    )
-                }
+                    }
+
+                )
+
             }
+        }
 
 
-        )
-    }
-//**************VALIDAR ESSE ROLE (ADICIONAR)********************************** */
-        // if(response.ok) {
+    )
+}
 
-        //     let skeletonRef = document.querySelector('#skeleton')
+// function concluirTarefas(){}
 
-        //     skeletonRef.style.display = 'none'
+//Carregar "pagina"
+window.addEventListener('load', () =>{
 
-        //     response.json().then(
-                
-        //         tasks => {
-        //             `<li class="tarefa">
-        //                 <div class="not-done"></div>
-        //                 <div class="descricao">
-        //                     <p class="nome">Nova tarefa</p>
-        //                     <p class="timestamp">Criada em: 15/07/21</p>
-        //                 </div>
-        //             </li>`
-        //         }
-        //     )
-
-        // } else {
-        //     if(response.status !== 401) {
-
-        //         logOutUser()
-
-        //     }
-        //}
+    getTasks()
+    
 })
+
+
+// //Requisição p/ atualizar tarefas para pendentes
+// const requestPutAuthorizateConfiguration = {
+//     method: 'PUT',
+//     headers:{
+//         'Content-Type':'application/json',
+//         'Authorization': localStorage.getItem('token')
+//     }
+// }
+
+// function updateTask(id, completed) {
+//     requestPutAuthorizateConfiguration.body = JSON.stringify({completed: completed})
+//     fetch(`https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`, requestPutAuthorizateConfiguration).then(
+//         response => {
+//             if(response.ok){
+//                 getTasks()
+//             }
+//         }
+//     )
+// }
+
+
+
+// //Requisição p/ deletar tarefas
+// const requestDeleteAuthorizateConfiguration = {
+//     method: 'DELETE',
+//     headers:{
+//         'Content-Type':'application/json',
+//         'Authorization': localStorage.getItem('token')
+//     }
+// }
+
+// function deleteTask(id) {
+//     fetch(`https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`, requestDeleteAuthorizateConfiguration).then(
+//         response => {
+//             if(response.ok){
+//                 getTasks()
+//             }
+//         }
+//     )
+// }
 
 
 
